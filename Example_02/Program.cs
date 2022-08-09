5df1a9a3bd45e886ad4ce995dfbe1833f8787a08
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-08-09 17:15:01 +0300

    second solution for task11

--- Example_02/Program.cs
+++ Example_02/Program.cs
@@ -2,6 +2,17 @@
 трёхзначное число и удаляет вторую цифру этого
 числа. */
 
+/* int num = new Random().Next(100, 1000);
+
+Console.WriteLine(num);
+
+int c1 = num / 100; // 123/100=1
+int c3 = num % 10; // 123%10=3
+
+Console.WriteLine(c1 + "" + c3); */
+
+
+//Second solution//////
 int num = new Random().Next(100, 1000);
 
 Console.WriteLine(num);
@@ -9,4 +20,4 @@ Console.WriteLine(num);
 int c1 = num / 100; // 123/100=1
 int c3 = num % 10; // 123%10=3
 
-Console.WriteLine(c1+""+c3);
+Console.WriteLine($"{c1}{c3}");
