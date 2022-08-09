ace7577d663b6dcb18fb42f9ca5f877eeb741a4f
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-08-09 16:32:48 +0300

    Task 9. create a programm

--- Example_01/Program.cs
+++ Example_01/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 9. Напишите программу, которая выводит
+случайное число из отрезка [10, 99] и показывает
+наибольшую цифру числа. */
+
+int num = new Random().Next(10, 100);
+
+Console.WriteLine(num);
+
+int c1 = num / 10; //12345/10=1234
+int c2 = num % 10; //12345%10=5
+
+if (c1 > c2)
+{
+    Console.WriteLine(c1);
+}
+else
+{
+    Console.WriteLine(c2);
+}
